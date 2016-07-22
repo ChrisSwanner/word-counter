15e2b2e627d0bf87bf3b18c206774e18eff44ad4
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2016-07-22 12:27:46 -0700

    add a post route to return results view, and add results view output with basic styling

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -11,6 +11,11 @@ namespace WordCounter
       Get["/"] = _ => {
         return View["index.cshtml"];
       };
+
+      Post["/results"] = _ => {
+        RepeatCounter newRepeatCounter = new RepeatCounter(Request.Form["user-word"], Request.Form["user-sentence"]);
+        return View["results.cshtml", newRepeatCounter];
+      };
     }
   }
 }
